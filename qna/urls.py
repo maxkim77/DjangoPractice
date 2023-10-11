667895dc6b06365ea1f5cd0c396a2e5e0188041a
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-10-11 16:30:12 +0900

    Delete qna directory

--- qna/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('qna/', views.qna_main, name='qna_main'), # accounts/login
-    path('qna/<int:pk>/', views.qna_contents, name='qna_contents'), # accounts/logout
-]
