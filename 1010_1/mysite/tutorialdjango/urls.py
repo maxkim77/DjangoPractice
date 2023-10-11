a98d25669fc1f03b8560dc0b1cd1c62119d22935
maxkim77 <maxkim7723@gmail.com>
2023-10-11 19:42:47 +0900

    commit

---
+++ 1010_1/mysite/tutorialdjango/urls.py
@@ -0,0 +1,9 @@
+#tutorialdjango > urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('blog/', include('blog.urls')),
+]
---
+++ 1011/mysite/main/views.py
@@ -0,0 +1,5 @@
+# main > views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
